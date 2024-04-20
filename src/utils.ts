6256beff318688e8731e6617befb8ca4e4d69a1a
mintaeKim <peterzzo@naver.com>
2024-04-20 15:01:42 +0900

    make img path change

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import noImg from "../public/image-not-found-scaled-1150x647.png"
 
 export function makeImagePath(id: string, format?: string) {
-    if(id == "" || id == null || id == undefined){
+    if(id === undefined || id === null){
         return noImg;
     }
     return `https://image.tmdb.org/t/p/${format ? format : "original"}/${id}`;
